a72b989b267df8b64509675eb206710de9d630c8
Manuel Santiago <manuel_sm223@hotmail.com>
2021-06-02 23:17:48 -0500

    ADD: grab movies from db

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,35 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import { selectMovies } from "../features/movie/movieSlice";
+import { useSelector } from "react-redux";
 
 function Movies() {
+  const movies = useSelector(selectMovies);
+  console.log(movies);
+
   return (
     <Container>
       <h4>Recommended for you</h4>
       <Content>
-        <Wrap>
-          <img src="https://cannonball-cdn.bamgrid.com/assets/originals/Mandalorian_S2.png" />
-        </Wrap>
-        <Wrap>
-          <img src="https://cannonball-cdn.bamgrid.com/assets/originals/Mandalorian_S2.png" />
-        </Wrap>
-        <Wrap>
-          <img src="https://cannonball-cdn.bamgrid.com/assets/originals/Mandalorian_S2.png" />
-        </Wrap>
-        <Wrap>
-          <img src="https://cannonball-cdn.bamgrid.com/assets/originals/Mandalorian_S2.png" />
-        </Wrap>
-        <Wrap>
-          <img src="https://cannonball-cdn.bamgrid.com/assets/originals/Mandalorian_S2.png" />
-        </Wrap>
-        <Wrap>
-          <img src="https://cannonball-cdn.bamgrid.com/assets/originals/Mandalorian_S2.png" />
-        </Wrap>
-        <Wrap>
-          <img src="https://cannonball-cdn.bamgrid.com/assets/originals/Mandalorian_S2.png" />
-        </Wrap>
-        <Wrap>
-          <img src="https://cannonball-cdn.bamgrid.com/assets/originals/Mandalorian_S2.png" />
-        </Wrap>
+        {movies &&
+          movies.map((movie) => (
+            <Wrap key={movie.id}>
+              <img src={movie.cardImg} />
+            </Wrap>
+          ))}
       </Content>
     </Container>
   );
